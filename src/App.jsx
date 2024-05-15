100e3889087e598db449f062cab3c23f80364053
Trinasust41 <trinasustcse41@gmail.com>
2024-05-15 16:40:06 +0600

    fifth

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { useContext, useEffect, useState } from "react";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import "./assets/css/app.css";
 import { Navbar } from "./components/Navbar";
 import { NoteCard } from "./components/NoteCard";
 import { NoteDetails } from "./components/NoteDetails";
 import { UpsertNote } from "./components/UpsertNote";
 import { PaletteContext } from "./context/PaletteContext";
+import LoadingSpinner from './components/LoadingSpinner';
 
 const palettes = [
   { id: 1, color: "#0d1282", name: "blue-palette" },
-  { id: 2, color: "#ff2171", name: "rose-palette" },
-  { id: 3, color: "#360d95", name: "violet-palette" },
+  { id: 2, color: "#F5BD02", name: "rose-palette" },
+  { id: 3, color: "#90EE90", name: "violet-palette" },
   { id: 4, color: "#333", name: "black-palette" },
 ];
 
@@ -20,12 +24,12 @@ export default function App() {
   const [notes, setNotes] = useState([]);
   const [currentNote, setCurrentNote] = useState(null);
   const [search, setSearch] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
   const [currentPalette, setCurrentPalette] = useState(
     state?.palette
       ? palettes.find((p) => p.id === state.palette.id)
       : palettes[0]
   );
-  let filteredNotes = [];
 
   useEffect(() => {
     const tempNotes = JSON.parse(localStorage.getItem("notes"));
@@ -37,11 +41,16 @@ export default function App() {
   };
 
   const handleCreateNote = (note) => {
-    if (note) {
-      const tempNotes = [...notes, note];
-      setNotes(tempNotes);
-      saveNotes(tempNotes);
-    }
+    setIsLoading(true);
+    setTimeout(() => {
+      if (note) {
+        const tempNotes = [...notes, note];
+        setNotes(tempNotes);
+        saveNotes(tempNotes);
+        toast.success('Note added successfully!');
+      }
+      setIsLoading(false);
+    }, 3000); // 3 seconds
   };
 
   const handleOnUpdate = (note) => {
@@ -50,18 +59,28 @@ export default function App() {
   };
 
   const handleUpdateNote = (note) => {
-    if (note) {
-      const tempNotes = [...notes.map((n) => (n.id === note.id ? note : n))];
-      setNotes(tempNotes);
-      setCurrentNote(null);
-      saveNotes(tempNotes);
-    }
+    setIsLoading(true);
+    setTimeout(() => {
+      if (note) {
+        const tempNotes = notes.map((n) => (n.id === note.id ? note : n));
+        setNotes(tempNotes);
+        setCurrentNote(null);
+        saveNotes(tempNotes);
+        toast.info('Note edited successfully!');
+      }
+      setIsLoading(false);
+    }, 3000); // 3 seconds
   };
 
   const handleDeleteNote = (noteId) => {
-    const tempNotes = [...notes.filter((n) => n.id !== noteId)];
-    setNotes(tempNotes);
-    saveNotes(tempNotes);
+    setIsLoading(true);
+    setTimeout(() => {
+      const tempNotes = notes.filter((n) => n.id !== noteId);
+      setNotes(tempNotes);
+      saveNotes(tempNotes);
+      toast.error('Note deleted successfully!');
+      setIsLoading(false);
+    }, 3000); // 3 seconds
   };
 
   const handleOnPreview = (note) => {
@@ -69,17 +88,11 @@ export default function App() {
     setOnViewNote(true);
   };
 
-  if (search) {
-    filteredNotes = [
-      ...notes.filter(
-        (n) =>
-          n.title.toLowerCase().includes(search.toLocaleLowerCase()) ||
-          n.desc.toLowerCase().includes(search.toLocaleLowerCase())
-      ),
-    ];
-  } else {
-    filteredNotes = [...notes];
-  }
+  const filteredNotes = notes.filter(
+    (n) =>
+      n.title.toLowerCase().includes(search.toLowerCase()) ||
+      n.desc.toLowerCase().includes(search.toLowerCase())
+  );
 
   return (
     <div
@@ -99,6 +112,7 @@ export default function App() {
         <div className="search-wrapper">
           <input
             onChange={(e) => setSearch(e.target.value)}
+            value={search}
             type="text"
             className="search-input"
             placeholder="Search"
@@ -107,17 +121,29 @@ export default function App() {
             <i className="fa-solid fa-magnifying-glass"></i>
           </button>
         </div>
-        <div className="notes-wrapper">
-          {filteredNotes.map((note) => (
-            <NoteCard
-              key={note?.id}
-              note={note}
-              onDelete={handleDeleteNote}
-              onUpdate={handleOnUpdate}
-              onPreview={handleOnPreview}
-            />
-          ))}
-        </div>
+        {isLoading ? (
+          <LoadingSpinner />
+        ) : (
+          <div className="notes-wrapper">
+            <TransitionGroup>
+              {filteredNotes.map((note) => (
+                <CSSTransition
+                  key={note.id}
+                  timeout={500}
+                  classNames="note"
+                >
+                  <NoteCard
+                    key={note?.id}
+                    note={note}
+                    onDelete={handleDeleteNote}
+                    onUpdate={handleOnUpdate}
+                    onPreview={handleOnPreview}
+                  />
+                </CSSTransition>
+              ))}
+            </TransitionGroup>
+          </div>
+        )}
         {onCreateNote && (
           <UpsertNote
             note={currentNote}
@@ -130,6 +156,7 @@ export default function App() {
           <NoteDetails note={currentNote} setView={setOnViewNote} />
         )}
       </div>
+      <ToastContainer />
     </div>
   );
 }
